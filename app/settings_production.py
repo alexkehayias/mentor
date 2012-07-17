649f619d155844e94a9b3218becdc0184b2d60c4
Alex Kehayias <alex.kehayias@gmail.com>
2012-07-17 16:59:16 +0200

    Updated prod settings.

--- app/settings_production.py
+++ app/settings_production.py
@@ -1,4 +1,8 @@
 from settings import *
+import json
+
+with open('/home/dotcloud/environment.json') as f:
+    env = json.load(f)
 
 SERVER_SCHEME_AND_NETLOC = 'http://mentorship.p2pu.org'
 SERVER_TYPE = 'production'
@@ -6,6 +10,18 @@ SERVER_TYPE = 'production'
 EMBED_ANALYTICS = True
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'template1',
+        'USER': env['DOTCLOUD_DB_SQL_LOGIN'],
+        'PASSWORD': env['DOTCLOUD_DB_SQL_PASSWORD'],
+        'HOST': env['DOTCLOUD_DB_SQL_HOST'],
+        'PORT': int(env['DOTCLOUD_DB_SQL_PORT']),
+        'OPTIONS': {'autocommit': True,}        
+    }
+}
 
 STATIC_ROOT = '/home/dotcloud/volatile/static/'
 STATIC_URL = '/static/'
